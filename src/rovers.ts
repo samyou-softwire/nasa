46f0b7408b2ff18b493fc85c25f9f776b155e3c2
Samuel Young <samuel.young@softwire.com>
2023-10-24 10:01:12 +0100

    specify type on get

--- src/rovers.ts
+++ src/rovers.ts
@@ -9,7 +9,7 @@ export function addRoverEndpoint(router: Router) {
     router.get('/rovers', async (req, res) => {
         console.log(API_KEY);
 
-        const response: AxiosResponse<Welcome, any> = await axios.get(
+        const response = await axios.get<Welcome>(
             `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${API_KEY}`
         )
 
